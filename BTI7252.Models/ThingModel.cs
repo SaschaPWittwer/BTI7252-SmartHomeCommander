4c988ba7db34e0a9e7f0c6eb08554a37f82d12fa
Lukas Läderach <lukas.laederach@3volutions.ch>
2018-12-07 15:03:36 +0100

    create data access layer for couchDB

--- BTI7252.Models/ThingModel.cs
+++ BTI7252.Models/ThingModel.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BTI7252.Models
 {
 	public class ThingModel
 	{
+		[Required]
 		public Guid ThingId { get; set; }
-		public string Descriptions { get; set; }
-		public DateTime Created { get; set; }
-		public DateTime Updated { get; set; }
 
+		[Required]
+		public string Description { get; set; }
+
+		public DateTime? Created { get; set; }
+		public DateTime? Updated { get; set; }
+
+		[Required]
 		public IEnumerable<ReadModel> Data { get; set; }
+
+		[Required]
 		public IEnumerable<EventModel> Events { get; set; }
 	}
 }
---
+++ BTI7252.Repositories/ICouchBucketManager.cs
@@ -0,0 +1,9 @@
+﻿using Couchbase.Core;
+
+namespace BTI7252.DataAccess
+{
+	public interface ICouchBucketManager
+	{
+		IBucket Open(string name);
+	}
+}

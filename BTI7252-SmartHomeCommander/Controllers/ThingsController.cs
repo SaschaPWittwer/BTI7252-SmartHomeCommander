5b5811dad0d606e9f23a6c225ebefc886a10141e
Lukas Läderach <lukas.laederach@3volutions.ch>
2018-12-07 11:21:45 +0100

    add register endpoint to api

--- BTI7252-SmartHomeCommander/Controllers/ThingsController.cs
+++ BTI7252-SmartHomeCommander/Controllers/ThingsController.cs
@@ -1,36 +1,45 @@
 using System.Threading.Tasks;
-using BTI7252_SmartHomeCommander.Models;
+using BTI7252.Models;
 using BTI7252_SmartHomeCommander.Models.Things;
-using BTI7252_SmartHomeCommander.Mqtt;
 using Microsoft.AspNetCore.Mvc;
-using MQTTnet.Client;
 
 namespace BTI7252_SmartHomeCommander.Controllers
 {
-    [Route("/api/[controller]")]
-    [ApiController]
-    public class ThingsController : ControllerBase
-    {
+	[Route("/api/[controller]")]
+	[ApiController]
+	public class ThingsController : ControllerBase
+	{
+		/// <summary>
+		///     Registers a new sensors metadata
+		/// </summary>
+		/// <param name="data">the sensor meta data</param>
+		/// <returns><code>Ok</code> if the sensor could be registered successfully, <code>Failure</code> otherwise</returns>
+		[HttpPost]
+		[ProducesResponseType(200)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(500)]
+		public async Task<ActionResult> PostAsync(ThingMetadata data)
+		{
+			return Ok();
+		}
 
-        /// <summary>
-        ///     Registers a new sensors metadata
-        /// </summary>
-        /// <param name="data">the sensor meta data</param>
-        /// <returns><code>Ok</code> if the sensor could be registered successfully, <code>Failure</code> otherwise</returns>
-        [HttpPost]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(500)]
-        public async Task<ActionResult> PostAsync(ThingMetadata data)
-        {
-            return Ok();
-        }
+		[HttpPost]
+		[Route("Register")]
+		public async Task<ActionResult> Register([FromBody]ThingModel model)
+		{
+			if (model == null)
+			{
+				return BadRequest();
+			}
 
-        [HttpGet]
-        public async Task<ActionResult> Get()
-        {
-            string result = "B========D";
-            return Ok(result);
-        }
-    }
+			return Ok();
+		}
+
+		[HttpGet]
+		public async Task<ActionResult> Get()
+		{
+			var result = "B========D";
+			return Ok(result);
+		}
+	}
 }

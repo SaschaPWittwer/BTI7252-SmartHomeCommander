fc0c1ba6613db1e0a67427edb911b013391be9cf
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2018-12-21 10:17:43 +0100

    fix stuff after merge

--- BTI7252-SmartHomeCommander/Startup.cs
+++ BTI7252-SmartHomeCommander/Startup.cs
@@ -11,68 +11,64 @@ using MQTTnet.Client;
 
 namespace BTI7252_SmartHomeCommander
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-			// Bootstrap shit
-			services.AddTransient<IMqttSender, MqttSender>();
-			services.AddTransient<IMqttConnectionManager, MqttConnectionManager>();
-			services.AddTransient<ICouchBucketManager, CouchBucketManager>();
-			services.AddTransient<ICouchConnectionManager, CouchConnectionManager>(c =>
-				new CouchConnectionManager(Configuration.GetValue<Uri>("Couchdb:Url"),
-					Configuration.GetValue<string>("Couchdb:Username"),
-					Configuration.GetValue<string>("Couchdb:Password")));
-			services.AddTransient<ICouchRepository, CouchRepository>(c =>
-				new CouchRepository(c.GetService<ICouchBucketManager>(),
-					Configuration.GetValue<string>("Couchdb:BucketName")));
+            // Bootstrap shit
+            services.AddTransient<IMqttSender, MqttSender>();
+            services.AddTransient<IMqttConnectionManager, MqttConnectionManager>();
+            services.AddTransient<ICouchBucketManager, CouchBucketManager>();
+            services.AddTransient<ICouchConnectionManager, CouchConnectionManager>(c =>
+                new CouchConnectionManager(Configuration.GetValue<Uri>("Couchdb:Url"),
+                    Configuration.GetValue<string>("Couchdb:Username"),
+                    Configuration.GetValue<string>("Couchdb:Password")));
+            services.AddTransient<ICouchRepository, CouchRepository>(c =>
+                new CouchRepository(c.GetService<ICouchBucketManager>(),
+                    Configuration.GetValue<string>("Couchdb:BucketName")));
 
-			// Create a new MQTT client.
-			var factory = new MqttFactory();
-			var mqttClient = factory.CreateMqttClient();
-			services.AddSingleton(mqttClient);
-			var mqttOptions = new MqttClientOptionsBuilder()
-				.WithClientId($"CommandApi-{Guid.NewGuid()}")
-				.WithCommunicationTimeout(TimeSpan.FromSeconds(Configuration.GetValue<int>("Mqtt:Timeout")))
-				.WithKeepAlivePeriod(TimeSpan.FromSeconds(100))
-				.WithCredentials(Configuration.GetValue<string>("Mqtt:User"),
-					Configuration.GetValue<string>("Mqtt:Password"))
-				.WithTcpServer(Configuration.GetValue<string>("Mqtt:Host"), 1883)
-				.Build();
+            // Create a new MQTT client.
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            services.AddSingleton(mqttClient);
+            var mqttOptions = new MqttClientOptionsBuilder()
+                .WithClientId($"CommandApi-{Guid.NewGuid()}")
+                .WithCommunicationTimeout(TimeSpan.FromSeconds(Configuration.GetValue<int>("Mqtt:Timeout")))
+                .WithKeepAlivePeriod(TimeSpan.FromSeconds(100))
+                .WithCredentials(Configuration.GetValue<string>("Mqtt:User"),
+                    Configuration.GetValue<string>("Mqtt:Password"))
+                .WithTcpServer(Configuration.GetValue<string>("Mqtt:Host"), 1883)
+                .Build();
 
-			services.AddSingleton(mqttOptions);
+            services.AddSingleton(mqttOptions);
 
-			services.AddSwaggerDocument();
-		}
+            services.AddSwaggerDocument();
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
             {
-				app.UseDeveloperExceptionPage();
-            }
-			else
-            {
-				app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
 
-			// Register the Swagger generator and the Swagger UI middlewares
-			app.UseSwagger();
-			app.UseSwaggerUi3();
 
-			app.UseHttpsRedirection();
-			app.UseMvc();
-		}
-	}
+            // Register the Swagger generator and the Swagger UI middlewares
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+
+            app.UseMvc();
+        }
+    }
 }
